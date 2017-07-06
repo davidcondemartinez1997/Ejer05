736a75b578e5d6003a977dfb43caf2e6eeb540df
dconde <davidcondemartinez@cic.es>
2017-07-06 17:40:57 +0200

    Primer commit

--- Ejer05/Class1.cs
+++ Ejer05/Class1.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Ejer05
 {
-    public class Class1
+    public class Data
     {
+        public string text = "Hola mundo";
+        public int number1 = 1;
+        public int number2 = 2;
+
+    }
+
+    public class Metodo : Data
+    {
+        Data datos = new Data();
+        public string Concatenar()
+        {
+            return datos.text + "-" + datos.number1 + "-" + datos.number2;
+        }
     }
 }
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ejer05;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Metodo metodo1 = new Metodo();
+            var res = metodo1.Concatenar();
+        }
+    }
+}
